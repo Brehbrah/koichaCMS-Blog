774ac2522e1fef776ae3368bbb54f91be48da2df
Khoi Hoang <hello@koicha.dev>
2019-12-22 16:19:00 +0100

    Added User Model & Email Checking
    - Checked if email exist from the database

---
+++ app/models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+    class User {
+        private $db;
+        
+        public function __construct() {
+            $this -> db = new Database;
+        }
+
+        // Find user by email
+        public function findUserByEmail($email) {
+            $this -> db -> query('SELECT * FROM users WHERE email = :email');
+            $this -> db -> bind(':email', $email);
+
+            $row = $this -> db -> single();
+
+            // Check row - If greater than zero, then there is no email found
+            if($this -> db -> rowCount() > 0 ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+?>
