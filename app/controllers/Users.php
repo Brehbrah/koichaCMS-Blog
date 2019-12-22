4fcc3f81e93dc8de7637cc0f6578a477230bb8fc
Khoi Hoang <hello@koicha.dev>
2019-12-22 17:40:32 +0100

    Added User Session Data & Logout
    Navbar will display differently when logged in vs logged out

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -134,7 +134,7 @@
                     $loggedInUser = $this -> userModel -> login($data['email'], $data['password']);
                     if($loggedInUser) {
                         // Create Session variables
-                        die('SUCCESS');
+                        $this -> createUserSession($loggedInUser);
                     } else {
                         $data['password_err'] = 'Password incorrect';
 
@@ -157,6 +157,28 @@
                 $this -> view('users/login', $data);
             }
         }
-    }
 
+        public function createUserSession($user) {
+            $_SESSION['user_id'] = $user -> id;
+            $_SESSION['user_email'] = $user -> email;
+            $_SESSION['user_name'] = $user -> name;
+            redirect('pages/index');
+        }
+
+        public function logout() {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+            redirect('users/login');
+        }
+
+        public function isLoggedIn() {
+            if(isset($_SESSION['user_id'])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 ?>
